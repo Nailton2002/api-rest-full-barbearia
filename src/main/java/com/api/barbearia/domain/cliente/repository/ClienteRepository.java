2714a8b75c1d621d7e5641bf73f5929286d4f4fe
Nailton2002 <jnmp25jnmp@gmail.com>
2023-06-02 07:49:26 -0300

    Criando a classe de Repository do Cliente.

---
+++ src/main/java/com/api/barbearia/domain/cliente/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.api.barbearia.domain.cliente.repository;
+
+import com.api.barbearia.domain.cliente.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Page<Cliente> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query(value = "select c from Cliente c where c.nome like %?1%")
+    List<Cliente> findByNome(String nome);
+}
