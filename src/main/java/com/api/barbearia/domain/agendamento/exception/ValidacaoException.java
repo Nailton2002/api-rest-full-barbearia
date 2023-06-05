956d3a44e51a9298dd94a264eff5a01ed23b2edb
Nailton2002 <jnmp25jnmp@gmail.com>
2023-06-05 14:35:46 -0300

    Criando o enum do cancelamento e a exceção para validações do agendamento.

---
+++ src/main/java/com/api/barbearia/domain/agendamento/exception/ValidacaoException.java
@@ -0,0 +1,5 @@
+package com.api.barbearia.domain.agendamento.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {super(mensagem);}
+}
