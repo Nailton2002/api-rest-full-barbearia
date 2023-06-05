2680d6fb154c4828295d0304c90e3dedf7f7f65a
Nailton2002 <jnmp25jnmp@gmail.com>
2023-06-05 18:40:37 -0300

    Criando a classe para validar se um barbeiro esta com agendamento no mesmo dia.

---
+++ src/main/java/com/api/barbearia/domain/agendamento/validations/agendamento/ValidadorBarbeiroComOutraAgendamentoNoMesmoHorario.java
@@ -0,0 +1,25 @@
+package com.api.barbearia.domain.agendamento.validations.agendamento;
+
+import com.api.barbearia.controller.agendamento.dto.AgendamentoDadosCadastro;
+import com.api.barbearia.domain.agendamento.exception.ValidacaoException;
+import com.api.barbearia.domain.agendamento.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorBarbeiroComOutraAgendamentoNoMesmoHorario implements ValidadorAgendamento{
+
+    @Autowired
+    private AgendamentoRepository repository;
+
+    public void validar(AgendamentoDadosCadastro dados){
+
+        var barbeiroPossuiOutraConsultaNoMesmoHorario = repository.existsByBarbeiroIdAndData(dados.idBarbeiro(),
+            dados.data());
+
+        if (barbeiroPossuiOutraConsultaNoMesmoHorario){
+            throw new ValidacaoException("Barbeiro já possui outra agendamento nesse mesmo horário!");
+        }
+    }
+
+}
