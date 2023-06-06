2a58ca4927f7d72ea21a81f83751134f35ba76eb
Nailton2002 <jnmp25jnmp@gmail.com>
2023-06-06 11:30:02 -0300

    Criando a clesse configuração de segurança para o profile de dev projeto.

---
+++ src/main/java/com/api/barbearia/infra/security/DevSecurityConfigurations.java
@@ -0,0 +1,26 @@
+package com.api.barbearia.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+public class DevSecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                   .requestMatchers("/**")
+                   .permitAll()
+                   .and()
+                   .csrf()
+                   .disable()
+                   .build();
+    }
+
+}
